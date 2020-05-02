3e974d4c21a0ad117335632c495a146723dd302c
Muhammet Yildiz <muhammetyildiz@gmail.com>
2020-05-02 04:56:37 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,6 +1,14 @@
-def test
-  a=[1,2,3,4}
-  a.pop 
+
+def print_all(array)
+  counter = 0
+
+  while counter < array.length do
+    puts array[counter]
+    counter += 1
+  end
 end
 
-puts test
+sayilar = [1,2,3,4,5]
+pets = ["1", "Cat", "2", "Bird", "Hamster"]
+print_all(pets)
+print_all(sayilar)
